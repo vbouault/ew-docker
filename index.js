7ba60d99b7b73927adedaf72b7ff50ff9f0937fb
vbouault <vbouault@hotmail.fr>
2021-01-05 12:26:36 +0100

    ci test

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ process.on("SIGTERM", () => {
 (async () => {
     const app = express();
 
-    console.log("Connecting to MongoDB");
+    console.log("Connecting to MongoDB, nice");
     await mongoose.connect("mongodb://mongodb:27017/ew-docker", { useUnifiedTopology: true, useNewUrlParser: true });
     console.log("Connected");
 
